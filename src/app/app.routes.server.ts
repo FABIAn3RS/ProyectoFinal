5e9f2ba462d999e472073804ef77361851433c5b
FABIAn3RS <balsecafabian4@gmail.com>
2026-01-01 17:53:29 -0500

    actualizacion de dependencias 2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,8 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
-  }
+  }, {
+    path: 'revista/:id',
+    renderMode: RenderMode.Server // <-- Esto le dice a Vercel: "No intentes crear esta página al compilar, créala cuando el usuario entre"
+  },
 ];
